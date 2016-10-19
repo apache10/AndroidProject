10e0fc562583d3caa2cb40d62376618d8e08ae1c
gaurav <gagu12ec@gmail.com>
2016-10-19 22:02:55 +0530

    done

--- StarWar/app/src/main/java/com/example/gaurav/starwar/constant/ProjectConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.gaurav.starwar.constant;
-
-/**
- * Created by gaurav on 17/10/16.
- */
-
-public class ProjectConstant {
-    public static final String GetStarWarApiUrl="http://swapi.co/api/";
-}
