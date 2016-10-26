aaacf33a4f59220f5825c828d18d23c3c3bb288f
gaurav <gagu12ec@gmail.com>
2016-10-26 22:28:14 +0530

    Using Camera

---
+++ UsingCamera/app/src/main/java/com/example/gaurav/usingcamera/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.gaurav.usingcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends ActionBarActivity {
+    Button b1,b2;
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        iv=(ImageView)findViewById(R.id.imageView);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        iv.setImageBitmap(bp);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
