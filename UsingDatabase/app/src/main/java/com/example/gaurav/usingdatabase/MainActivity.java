93006483d3c7de65a5a251f14d78a5d8ee702cf3
gaurav <gagu12ec@gmail.com>
2016-10-28 20:16:32 +0530

    Basic UI is created for project

---
+++ UsingDatabase/app/src/main/java/com/example/gaurav/usingdatabase/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.gaurav.usingdatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button logIn = (Button)findViewById(R.id.login);
+        Button signUp = (Button)findViewById(R.id.signup);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.log_in);
+                Toast.makeText(getApplicationContext(), "logIn", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.sign_up);
+                Toast.makeText(getApplicationContext(), "SingUp", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
